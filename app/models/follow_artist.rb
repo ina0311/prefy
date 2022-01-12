a8fb4e54f2a152f564fda1d764fd86ae16b8b121
ina0311 <numberg0311@gmail.com>
2022-01-12 13:38:57 +0900

    Fix: followartistを一括で更新するように変更

--- app/models/follow_artist.rb
+++ app/models/follow_artist.rb
@@ -7,31 +7,35 @@ class FollowArtist < ApplicationRecord
   def self.list_update(params, user)
     default_follow_artist_ids = user.follow_artist_lists.pluck(:spotify_id)
     now_follow_artist_ids = params.pluck(:id)
-    new_follow_artist_ids = now_follow_artist_ids - default_follow_artist_ids
-
     unfollow_artists_ids = default_follow_artist_ids - now_follow_artist_ids
-    FollowArtist.unfollow(unfollow_artists_ids, user) if unfollow_artists_ids.present?
+    new_follow_artist_ids = now_follow_artist_ids - default_follow_artist_ids
 
     Artist.all_update(params)
 
-    binding.pry
-    FollowArtist.all_follow(new_follow_artist_ids, user)
-    binding.pry
+    if unfollow_artists_ids.present?
+      unfollow_artists = Artist.where(spotify_id: unfollow_artist_ids).ids
+      FollowArtist.unfollow_all(unfollow_artists, user)
+    end
+
+    if new_follow_artist_ids.present?
+      new_follow_artists = Artist.where(spotify_id: new_follow_artist_ids).ids
+      FollowArtist.follow_all(new_follow_artists, user)
+    end
   end
 
   def self.follow(artist, user)
     follow_artist = user.follow_artists.create(artist_id: artist.id) 
   end
 
-  def self.unfollow(unfollow_artists, user)
-    unfollow_artists.each { |id| user.follow_artist_lists.delete(spotify_id: id) }
+  def self.unfollow_all(unfollow_artists, user)
+    user.follow_artists.destroy_all(spotify_id: id)
   end
 
-  def self.all_follow(follow_artist_ids, user)
-    follow_artist_attributes = follow_artist_ids.map { |id| {user_id: user.id, artist_id: id} }
-    
-    binding.pry
-    
-    FollowArtist.insert_all(follow_artist_attributes, record_timestamps: true)
+  def self.follow_all(follow_artists, user)
+    follow_artist_attributes = follow_artists.map do |id| 
+                                {user_id: user.id, artist_id: id, created_at: Time.current, updated_at: Time.current}
+                              end
+
+    FollowArtist.insert_all(follow_artist_attributes)
   end
 end
