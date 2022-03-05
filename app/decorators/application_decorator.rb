ea772bcc6a0e768963fab74501cc23f052544b53
ina0311 <numberg0311@gmail.com>
2022-03-05 10:54:10 +0900

    Fix: aritst_imageをdecorateで処理

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,16 +1,12 @@
 class ApplicationDecorator < Draper::Decorator
-  # Define methods for all decorated objects.
-  # Helpers are accessed through `helpers` (aka `h`). For example:
-  #
-  #   def percent_amount
-  #     h.number_to_percentage object.amount, precision: 2
-  #   end
   def image
     case
     when object.image.present?
       object.image
     when object.respond_to?(:album)
       album.image
+    else
+      'default_image.png'
     end
   end
 end
