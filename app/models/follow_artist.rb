a494fc69773f438e47a799149dfe5b65ab7e9507
ina0311 <numberg0311@gmail.com>
2022-03-08 12:20:20 +0900

    Add: ゲストユーザーのアーティストフォロー機能追加

--- app/models/follow_artist.rb
+++ app/models/follow_artist.rb
@@ -14,8 +14,8 @@ class FollowArtist < ApplicationRecord
 
   def self.follow_all(new_follow_artists, user)
     follow_artists = new_follow_artists.map do |id| 
-                                {user_id: user.spotify_id, artist_id: id, created_at: Time.current, updated_at: Time.current}
-                              end
+                       {user_id: user.spotify_id, artist_id: id, created_at: Time.current, updated_at: Time.current}
+                     end
     FollowArtist.insert_all(follow_artists)
   end
 end
--- app/services/users/artist_follower.rb
+++ app/services/users/artist_follower.rb
@@ -9,10 +9,20 @@ class Users::ArtistFollower < SpotifyService
   end
 
   def follow
-    response = request_artist_follow(@user, @artist_id)
-    if response == 204
-      Artists::ArtistRegistrar.call([@artist_id]) unless Artist.find_by(spotify_id: @artist_id)
-      FollowArtist.create!(user_id: @user.spotify_id, artist_id: @artist_id)
+    if @user.guest_user?
+      create_follow_artist!
+    else
+      response = request_artist_follow(@user, @artist_id)
+      if response == 204
+        create_follow_artist!
+      end
     end
+  end
+
+  private
+
+  def create_follow_artist!
+    Artists::ArtistRegistrar.call([@artist_id]) unless Artist.find_by(spotify_id: @artist_id)
+    FollowArtist.create!(user_id: @user.spotify_id, artist_id: @artist_id)
   end
 end
