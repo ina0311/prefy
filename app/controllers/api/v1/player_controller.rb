d7001809057e992841ff4525cd743dbe6feb5caa
ina0311 <numberg0311@gmail.com>
2022-03-16 22:28:49 +0900

    Add: playerの再生と一時停止を追加、再度再生するときにpositionが必要になるのでpositionカラムをplaylist_of_tracksに追加する

--- app/controllers/api/v1/player_controller.rb
+++ app/controllers/api/v1/player_controller.rb
@@ -1,19 +1,54 @@
 class Api::V1::PlayerController < ApplicationController
   def play
+    request_body = create_request_body(context_uri: player_params)
+    Players::TrackStarter.call(current_user, current_player, request_body)
+    
     session[:player] = 'active'
     session[:playing] = true
   end
 
   def pause
+    Players::TrackPause.call(current_user, current_player)
     session[:playing] = false
   end
 
   def start
+    response = Players::PlaybackStateGetter.call(current_user)
+    # positionをplaylist_of_tracksテーブルに追加する
+    request_body = create_request_body(context_uri: response[:uri], position_ms: response[:position_ms])
+    Players::TrackStarter.call(current_user, current_player, request_body)
     session[:playing] = true
   end
 
   def close    
     session.delete(:player)
     session.delete(:playing)
+  end
+
+  private
+
+  def player_params
+    params.require(:uri)
+  end
+
+  def current_player
+    get_available_devices if session[:device].nil?
+    @current_player ||= session[:device]
+  end
+
+  def get_available_devices
+    device = Players::DeviceGetter.call(@current_user)
+    session[:device] = device[:id]
+  end
+
+  def create_request_body(*arg)
+    arg = arg.extract_options!
+    {
+      context_uri: arg[:context_uri],
+      offset: { 
+        position: arg[:position] || 0 
+      },
+      position_ms: arg[:position_ms] || 0
+    }
   end
 end
---
+++ app/services/players/track_starter.rb
@@ -0,0 +1,23 @@
+class Players::TrackStarter < SpotifyService
+  def self.call(user, device, request_body)
+    new(user, device, request_body).start
+  end
+
+  def initialize(user, device, request_body)
+    @user = user
+    @device = device
+    @request_body = request_body
+  end
+
+  def start
+    request_player_start
+  end
+
+  private
+
+  def request_player_start
+    conn_request.put("me/player/play?device_id=#{@device}") do |req|
+      req.body = @request_body.to_json
+    end
+  end
+end
