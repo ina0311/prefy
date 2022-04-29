468f732392b898520ed5baf0a8072f9a48c79cee
inaba <numberg0311@gmail.com>
2022-04-29 11:47:45 +0900

    Fix: 検索を日本語を含めず、英語の名前から取得した名前で曲を検索する

--- app/models/saved_playlist.rb
+++ app/models/saved_playlist.rb
@@ -43,21 +43,23 @@ class SavedPlaylist < ApplicationRecord
     delete_saved_playlists.delete_all
   end
 
-  # saved_playlistの属性をクエリを作るためのフィルターに変換する
-  def convert_fillter
-    artists = self.get_artists if self.only_follow_artist.present?
-    targets = self.include_artists if self.include_artists.present?
-    period = self.that_generation_preference? ? convert_generation_to_period : self.period
-
-    return { artists: artists, period: period, targets: targets}
+  def convert_year
+    case 
+    when self.that_generation_preference.present?
+      return " year:#{self.convert_generation_to_period}"
+    when self.period.present?
+      return " year:#{self.period}"
+    else
+      nil
+    end
   end
 
   # ジャンルが指定されていればフォローアーティストを絞り込み検索する
-  def get_artists
+  def get_artist_ids
     if self.genres.present?
-      self.user.follow_artist_lists.includes(:artist_genre_lists).search_genre_names(self.genres.only_names)
+      self.user.follow_artist_lists.includes(:artist_genre_lists).search_genre_names(self.genres.only_names).ids
     else
-      self.user.follow_artist_lists
+      self.user.follow_artist_lists.ids
     end
   end
 
@@ -88,22 +90,11 @@ class SavedPlaylist < ApplicationRecord
     end
   end
 
-  # フィルターをクエリに変換する
-  def convert_querys(fillter)
-    string = String.new
-    string += "year:#{fillter[:period]}" if fillter[:period].present?
-    querys = fillter[:artists].present? ? add_artists(string, fillter[:artists]) : string
-    target_querys = add_artists(string, fillter[:targets]) if fillter[:targets].present?
-
-    return querys, target_querys
-  end
-
-  # クエリにアーティストを加える
-  def add_artists(string, artists)
-    artists.map do |artist|
-      copy_str = string.dup
-      copy_str += " artist:#{artist[:name]}"
-      {query: copy_str, artist_spotify_id: artist[:spotify_id]}
+  def refine_tracks(ramdom_tracks, target_tracks)
+    if self.max_total_duration_ms
+      return self.refine_by_duration_ms(ramdom_tracks, target_tracks)
+    else
+      return self.refine_by_max_number_of_track(ramdom_tracks, target_tracks)
     end
   end
 
@@ -111,39 +102,47 @@ class SavedPlaylist < ApplicationRecord
   def refine_by_max_number_of_track(ramdom_tracks, target_tracks)
     total = self.max_number_of_track.present? ? self.max_number_of_track : DEFAULT
     if target_tracks.present?
-      refined_target_tracks = target_tracks.map { |tg_tracks| tg_tracks.sample(total * PERCENTAGE) }.flatten     
-      remaining = total - refined_target_tracks.size
+      refined_target_tracks = target_tracks.map { |tg_tracks| tg_tracks.sample(total * PERCENTAGE) }
+      remaining = total - refined_target_tracks.flatten.size
+      binding.pry
       refined_ramdom_tracks = ramdom_tracks.sample(remaining)
-      return {refined_ramdom_tracks: refined_ramdom_tracks, refined_target_tracks: refined_target_tracks}
+      return refined_ramdom_tracks, refined_target_tracks
     else
-      return {refined_ramdom_tracks: ramdom_tracks.sample(total)}
+      return ramdom_tracks.sample(total)
     end
   end
 
   # 再生時間で絞り込む
   def refine_by_duration_ms(ramdom_tracks, target_tracks)
-    playlist_of_tracks = []
     if target_tracks
       limit = self.max_total_duration_ms * PERCENTAGE
-      refined_target_tracks = target_tracks.map { |tg_tracks| check_total_duration_and_add_tracks(limit, tg_tracks)}.flatten
+      refined_target_tracks = target_tracks.map { |tg_tracks| refine_total_duration_and_add_tracks(limit, tg_tracks)}.flatten
       remaining = self.max_total_duration_ms - refined_target_tracks.pluck(:duration_ms).sum
-      refine_ramdom_tracks = check_total_duration_and_add_tracks(remaining, ramdom_tracks)
-      return {refined_ramdom_tracks: refine_ramdom_tracks, refined_target_tracks: refined_target_tracks}
+      refine_ramdom_tracks = refine_total_duration_and_add_tracks(remaining, ramdom_tracks)
+      return refine_ramdom_tracks, refined_target_tracks
     else
-      return {refined_ramdom_tracks: check_total_duration_and_add_tracks(limit, ramdom_tracks)}
+      return refine_total_duration_and_add_tracks(limit, ramdom_tracks)
     end
   end
 
   # 再生時間を判定し、追加
-  def check_total_duration_and_add_tracks(limit, tracks)
-    playlist_of_tracks = []
-    tracks.shuffle!.each do |track|
-      playlist_of_tracks << track
-      limit -= track[:duration_ms]
-      break if limit <= 0
-    end
+  def refine_total_duration_and_add_tracks(limit, tracks)
+    refine_tracks = tracks.shuffle!.map do |track|
+                      playlist_of_tracks << track
+                      limit -= track[:duration_ms]
+                      break if limit <= 0
+                    end
 
-    return playlist_of_tracks
+    return refine_tracks
+  end
+
+  def has_track_by_require_artists(tracks)
+    track_artist_ids = tracks.flatten.map { |track| track[:artist_id] }.uniq.flatten
+    not_get_artist = self.include_artists.map do |artist|
+                       next if track_artist_ids.include?(artist.spotify_id)
+                       Artist.find_by(spotify_id: artist.spotify_id).name
+                     end
+    return not_get_artist.compact
   end
 
   def number_of_track_less_than_requirements?
--- app/services/saved_playlists/based_on_saved_playlist_tracks_getter.rb
+++ app/services/saved_playlists/based_on_saved_playlist_tracks_getter.rb
@@ -1,72 +1,87 @@
 class SavedPlaylists::BasedOnSavedPlaylistTracksGetter < SpotifyService
   # saved_playlistの情報からクエリを作成、曲を取得、絞り込み
-  def self.call(saved_playlist)
-    new(saved_playlist).get
+  def self.call(saved_playlist, user)
+    new(saved_playlist, user).get
   end
 
-  def initialize(saved_playlist)
+  def initialize(saved_playlist, user)
     @saved_playlist = saved_playlist
+    @user = user
   end
   
   def get
-    fillter = saved_playlist.convert_fillter
-    querys, target_querys = saved_playlist.convert_querys(fillter)
-    ramdom_tracks = search_tracks(querys).flatten
-    target_tracks = search_tracks(target_querys) if target_querys.present?
+    year = saved_playlist.convert_year
+    if saved_playlist.only_follow_artist
+      artist_ids = saved_playlist.get_artist_ids
+      ramdom_tracks = search_tracks(artist_ids, year).flatten
+    end
+
+    if saved_playlist.include_artists
+      target_ids = saved_playlist.include_artists.ids 
+      target_tracks = search_tracks(target_ids, year)
+    end
+    
     return false if ramdom_tracks.nil? && target_tracks.nil?
 
-    refined_ramdom_and_target_tracks = refine_tracks(ramdom_tracks, target_tracks)
+    refined_ramdom_and_target_tracks = saved_playlist.refine_tracks(ramdom_tracks, target_tracks)
     return refined_ramdom_and_target_tracks
   end
 
   private
 
-  attr_reader :saved_playlist
+  attr_reader :saved_playlist, :user
 
-  def search_tracks(querys)
-    response = request_search_tracks(querys)
-    return response if response.nil?
-
-    tracks = response_convert_tracks(response)
+  def search_tracks(ids, year)
+    request_params = create_request_params(ids, year)
+    tracks = request_search_tracks(request_params)
     return tracks
   end
 
-  def request_search_tracks(querys)
-    tracks = []
-    querys.each do |query|
-      response = RSpotify::Base.search(query[:query], 'track', limit: 50)
-      artist_tracks = []
-      response.each do |res|
-        # アルバムのタイプがコンピレーション、または違うアーティストの曲は弾く
-        next if res.album.album_type == 'compilation' || res.artists.map { |artist| artist.id != query[:artist_spotify_id] }.all?
-        artist_tracks << res
-      end
-      tracks << artist_tracks.compact
+  def create_request_params(ids, year)
+    offset = INITIAL_VALUE
+    response = Array.new 
+    while true
+      response.concat(RSpotify::Artist.find(ids[offset, 50]))
+      break if ids.size == response.size
+      offset += PLUS_FIFTY
     end
-    tracks = nil unless tracks.flatten.any?
+    request_params = response.map do |res|
+                      string = "artist:#{res.name}"
+                      string += year if year
+                      {id: res.id, params: URI.encode_www_form_component(string)}
+                     end
+    
+    return request_params
+  end
+
+  def request_search_tracks(request_params)
+    tracks = request_params.map do |req|
+               response = conn_request.get("search?q=#{req[:params]}&type=track&limit=50").body[:tracks][:items]
+               next if response.blank?
+               artist_tracks = response.map do |res|
+                                 next nil if should_remove_response?(res, req[:id])
+                                 track = {
+                                  spotify_id: res[:id],
+                                  name: res[:name],
+                                  duration_ms: res[:duration_ms],
+                                  artist_id: res[:artists].pluck(:id)
+                                }
+                               end
+                artist_tracks.compact.uniq { |track| track[:name] }
+             end
+    tracks.reject!(&:blank?)
     return tracks
   end
 
-  def response_convert_tracks(response)
-    tracks = []
-    response.each do |res|
-      next if res.blank?
-      artist_tracks = []
-      res.each do |r|
-        # アーティストの同じ名前の曲を弾く
-        next if artist_tracks.pluck(:name).include?(r.name)
-        artist_tracks << {track_id: r.id, name: r.name, duration_ms: r.duration_ms}
-      end
-      tracks << artist_tracks
-    end
-    return tracks
-  end
-
-  def refine_tracks(ramdom_tracks, target_tracks)
-    if saved_playlist.max_total_duration_ms.present?
-      return saved_playlist.refine_by_duration_ms(ramdom_tracks, target_tracks)
+  # アルバムのタイプがコンピレーション、または違うアーティストの曲は弾く
+  def should_remove_response?(response, request_artist_id)
+    case 
+    when response[:album][:album_type] == 'compilation'
+      return true
+    when response[:artists].map { |artist| artist[:id] != request_artist_id }.all?
+      return true
     else
-      return saved_playlist.refine_by_max_number_of_track(ramdom_tracks, target_tracks)
+      return false
     end
   end
 end
