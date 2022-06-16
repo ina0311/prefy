afec6df081381c0a5c84712a6ba593ccb94f7620
inaba <numberg0311@gmail.com>
2022-06-16 11:24:46 +0900

    fix: 曲の再生だけして、spotifyに飛ぶように変更

--- app/services/players/track_starter.rb
+++ app/services/players/track_starter.rb
@@ -1,25 +1,45 @@
 class Players::TrackStarter < SpotifyService
-  def self.call(user, device, request_body)
-    new(user, device, request_body).start
+  def self.call(user, device, type, id)
+    new(user, device, type, id).start
   end
 
-  def initialize(user, device, request_body)
+  def initialize(user, device, type, id)
     @user = user
     @device = device
-    @request_body = request_body
+    @type = type
+    @id = id
   end
 
   def start
-    request_player_start
+    binding.pry
+    uri = "spotify:#{type}:#{id}"
+    request_body = create_request_body(uri)
+    request_player_start(request_body)
   end
 
   private
 
-  attr_reader :user, :device, :request_body
+  attr_reader :user, :device, :type, :id
 
-  def request_player_start
+  def request_player_start(request_body)
     conn_request.put("me/player/play?device_id=#{device}") do |req|
       req.body = request_body.to_json
+    end
+  end
+
+  def create_request_body(uri)
+    if type == 'playlist'
+      {
+        context_uri: uri,
+        offset: { 
+          position: 0 
+        },
+        position_ms: 0
+      }
+    elsif type == 'track'
+      {
+        context_uri: uri
+      }
     end
   end
 end
